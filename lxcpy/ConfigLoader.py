2e3364db1fd86f6ad948128f84932896d798d660
sahe922a <everardo.sanchezh@sat.gob.mx>
2019-10-17 13:34:57 -0500

    feat: add tests

--- lxcpy/ConfigLoader.py
+++ lxcpy/ConfigLoader.py
@@ -1,3 +1,8 @@
+"""
+This module helps to loads and parse a json config file
+"""
+
+
 import json
 from .ConfigValidator import validate
 from .Utils import exit
@@ -10,16 +15,19 @@ def loads_json(file_path=""):
     Reads a config.json file and validates it
     Returns a valid dict
     """
-    file = open(file_path, "r")
-    data = file.read()
     try:
-        dict_data = json.loads(data)
-        is_valid = validate(required, dict_data, raise_exception=True)
-        return dict_data
-    except json.decoder.JSONDecodeError as e:
-        error_msg = "Error en el archivo {} ...\n\n{}".format(
-            file_path, str(e)
-        )
-        exit(error_msg)
-    except Exception as e:
+        file = open(file_path, "r")
+        data = file.read()
+        try:
+            dict_data = json.loads(data)
+            is_valid = validate(required, dict_data, raise_exception=True)
+            return dict_data
+        except json.decoder.JSONDecodeError as e:
+            error_msg = "Error en el archivo {} ...\n\n{}".format(
+                file_path, str(e)
+            )
+            exit(error_msg)
+        except Exception as e:
+            exit(str(e))
+    except FileNotFoundError as e:
         exit(str(e))
--- test/test_validator.py
+++ test/test_validator.py
@@ -2,27 +2,31 @@
 import unittest
 
 # Modules
-from PYLXC.ConfigValidator import validate
+from lxcpy.ConfigValidator import validate
 
 required = ["name", "image", "componentsPath"]
+
+bad_dict = {
+    "name": "some",
+    "componentsPath": "some"
+}
+good_dict = {
+    "name": "some",
+    "image": "some",
+    "componentsPath": "some"
+}
 
 
 class TestValidator(unittest.TestCase):
     def test_required(self):
-        dict_data = {
-            "name": "some",
-            "image": "some",
-            "componentsPath": "some"
-        }
 
-        res = validate(required, dict_data)
+        res = validate(required, good_dict)
         self.assertTrue(res)
 
     def test_required_fails(self):
-        dict_data = {
-            "name": "some",
-            "componentsPath": "some"
-        }
-
-        res = validate(required, dict_data)
+        res = validate(required, bad_dict)
         self.assertFalse(res)
+
+    def test_required_fails_raise(self):
+        with self.assertRaises(Exception):
+            validate(required, bad_dict, True)
